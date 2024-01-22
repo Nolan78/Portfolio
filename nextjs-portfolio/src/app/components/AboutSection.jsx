1567671b043ebf7d02adc48e30dc87e8eda97cfd
Nolan78 <superpika.nolan@gmail.com>
2024-01-22 20:33:16 +0100

    INIT

---
+++ nextjs-portfolio/src/app/components/AboutSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SQL Server</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>PHP</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Mastère 1 Développement Web et Mobile, Devops</li>
+        <li>Mastère 2 Développement, data & IA</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="apropos">
+      <div class="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -right-4 transform translate-x-1/2 -translate-1/2"></div>
+      <div className="md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image2.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">A propos</h2>
+          <p className="text-base lg:text-lg">
+            Je suis un développeur web full stack avec une passion pour la
+            création d'applications web interactives et réactives. J'ai de
+            l'expérience de travail avec JavaScript, React, Node.js, Express, SQL Server,
+            HTML et CSS. J'apprends vite et je cherche toujours à élargir mes
+            connaissances et mes compétences. Je suis un joueur d'équipe et je
+            suis ravi de travailler avec d'autres pour créer des applications
+            incroyables.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Compétences{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
